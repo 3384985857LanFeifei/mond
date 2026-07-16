1e0b52b0e3189c5015fa34489c30e5918f7da8db
roooot <103732419+rooootdev@users.noreply.github.com>
2026-07-16 02:09:27 +0200

    Initial Commit

---
+++ mond/mondApp.swift
@@ -0,0 +1,17 @@
+//
+//  mondApp.swift
+//  mond
+//
+//  Created by ruter on 16.07.26.
+//
+
+import SwiftUI
+
+@main
+struct mondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
